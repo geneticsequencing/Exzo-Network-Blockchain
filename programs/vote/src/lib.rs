96eac03a88d5cdfe51e12a9fc53f3bdf535b560a
topdefi <94203269+topdefi@users.noreply.github.com>
2023-03-06 15:43:29 -0800

    Update guide and staking requirements

--- programs/vote/src/lib.rs
+++ programs/vote/src/lib.rs
@@ -15,8 +15,8 @@ extern crate solana_frozen_abi_macro;
 pub use solana_sdk::vote::program::{check_id, id};
 
 
-/// Amount of stake to be in majority = 1M
-pub const MIN_STAKERS_TO_BE_MAJORITY: u64 = 1_000_000 * solana_sdk::native_token::LAMPORTS_PER_XZO;
+/// Amount of stake to be in majority = 10k
+pub const MIN_STAKERS_TO_BE_MAJORITY: u64 = 10_000 * solana_sdk::native_token::LAMPORTS_PER_XZO;
 
 /// Number of stakers with lamports more than 1M, to start filtering = 19
 pub const NUM_MAJOR_STAKERS_FOR_FILTERING: usize = 19;
